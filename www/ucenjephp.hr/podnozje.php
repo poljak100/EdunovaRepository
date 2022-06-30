de117b627de23c22574cbf3ceba4dc33f9fa08fb
Dino Poljak <104674307+poljak100@users.noreply.github.com>
2022-06-30 20:55:22 +0200

    Delete www/ucenjephp.hr directory

--- www/ucenjephp.hr/podnozje.php
+++
@@ -1,9 +0,0 @@
-<!-- Start podnožje -->
-<div class="grid-x">
-      <div class="cell">
-        <div class="callout">
-          &copy; Edunova 2022
-        </div>
-      </div>
-    </div>
-    <!-- End podnožje -->
