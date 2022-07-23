50914a20bf95e6193d2463423ecc2e4fa08d95e1
Dino <104674307+poljak100@users.noreply.github.com>
2022-07-23 14:28:23 +0200

    Update tvrtka.sql

--- zavrsni/tvrtka.sql
+++ zavrsni/tvrtka.sql
@@ -26,19 +26,18 @@ create table distribucija (
     mjesto varchar(50),
     vrijeme datetime,
     kolicina dec,
-    putnik int not null
+    osoba int not null
 );
 
-create table putnik (
+create table osoba (
     sifra int not null primary key auto_increment,
     ime varchar(50) not null,
     prezime varchar(50) not null,
-    mjesto varchar(50),
+    mjesto_stanovanja varchar(50),
     oib char (11),
     datum_rodenja datetime,
     naziv_terena varchar(50),
-    smjena varchar(50),
-    radno_vrijeme datetime
+    smjena varchar(50)   
 );
 
 create table proizvod_distribucija(
@@ -48,12 +47,32 @@ create table proizvod_distribucija(
 );
 
 
-alter table distribucija  add foreign key (putnik) references putnik (sifra);
+alter table distribucija  add foreign key (osoba) references osoba (sifra);
 alter table proizvod add foreign key (poslovnica) references poslovnica (sifra);
 
 alter table proizvod_distribucija add foreign key (proizvod) references proizvod (sifra);
 alter table proizvod_distribucija add foreign key (distribucija) references distribucija (sifra);
 
+
+insert into osoba (sifra ,ime ,prezime ,mjesto_stanovanja ,oib ,datum_rodenja ,naziv_terena ,smjena )
+values  (1,'Marko','Perić','Osijek',283927475829,'1988-02-01','Osijek','Poslijepodne'),
+        (2,'Ivana','Mandić','Osijek',81234029387,'1993-04-06','Osijek','Poslijepodne'),
+        (3,'Dea','Horvat','Tenja',98210092234,'1969-07-05','Našice','Prijepodne'),
+        (4,'Ivan','Hodak','Vukovar',99210036472,'1999-04-04','Vukovar','Prijepodne'),
+        (5,'Matej','Kolić','Brijest',83920109384,'1995-06-06','Slatina','Poslijepodne'),
+        (6,'Sonja','Galić','Višnjevac',00938741934,'1978-03-05','Osijek','Prijepodne'),
+        (7,'Dino','Majer','Bilje',0394718273921,'1994-01-01','Baranja','Poslijepodne'),
+        (8,'Ema','Krešo','Antunovac',938572819384,'1988-02-01','Osijek','Poslijepodne'),
+        (9,'Kristijan','Karlak','Osijek',53928333847,'1971-09-04','Osijek','Prijepodne'),
+        (10,'Marko','Tomić','Đakovo',19382634442,'1991-12-03','Đakovo','Poslijepodne'),
+        (11,'Siniša','Modrić','Bizovac',39843029385,'1994-02-03','Osijek','Prijepodne'),
+        (12,'Marta','Kazo','Tenja',7382930293845,'1998-11-04','Požega','Prijepodne');
+
+
+insert into poslovnica (sifra ,naziv ,mjesto ,email )
+values (1,'Ledo', 'Osijek','LedoOsijek@gmail.hr');
+
+insert into proizvod (sifra ,naziv_proizvoda ,rok_trajanja ,cijena_proizvoda ,proizvodac ,poslovnica )
 
 
 
